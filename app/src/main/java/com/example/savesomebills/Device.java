cff8734803e486eb66b45e29a78f5e2eb805b6a2
paul <a12433214@unet.univie.ac.at>
2026-06-04 11:21:18 +0200

    elements in device and group addeed

--- app/src/main/java/com/example/savesomebills/Device.java
+++ app/src/main/java/com/example/savesomebills/Device.java
@@ -11,8 +11,10 @@ public class Device {
     public int standbyHours;
     public int wattOn;
     public int wattStandby;
+    public String usage;
+    public String cost;
 
-    public Device(String name, String groupId, int onHours, int standbyHours, int wattOn, int wattStandby, String icon) {
+    public Device(String name, String groupId, int onHours, int standbyHours, int wattOn, int wattStandby, String icon,String usage,String cost) {
         this.objectId = UUID.randomUUID().toString();
         this.groupId = groupId;
         this.name = name;
@@ -21,5 +23,7 @@ public class Device {
         this.standbyHours = standbyHours;
         this.wattOn = wattOn;
         this.wattStandby = wattStandby;
+        this.usage = usage;
+        this.cost = cost;
     }
 }
